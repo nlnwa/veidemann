117ffe40eb71d170c4991cae08802cc9d9db05a7
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2021-09-28 14:39:19 +0200

    dev: configure scylla-operator to use only 1 replica

--- dev/scylla-operator/install_scylla_operator.sh
+++ dev/scylla-operator/install_scylla_operator.sh
@@ -6,6 +6,7 @@ helm repo update
 helm upgrade scylla-operator scylla-operator/scylla-operator \
 --install \
 --create-namespace \
+-f values.yaml \
 --namespace scylla-operator
 
 kubectl wait --for condition=established crd/scyllaclusters.scylla.scylladb.com
